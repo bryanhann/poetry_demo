21d80fdb25eb0035720801d8a3541b895dc49109
Bryan Hann <nobody@nowhere>
2020-11-21 14:58:57 +0800

    *Undocumented Changes*

--- .workon/workon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# PRECONDITION: ${bhw} is the root
-
-[ "$1" = "--auto" ] || source ${bhw}/.workon/warn.sh
-[ "$1" = "--auto" ] && shift
-[ true            ] && export BHW=${bhw}
-[ true            ] && source ${BHW}/.workon/activate
-echo bye!
-#source $BHW_ROOT/activate.sh
